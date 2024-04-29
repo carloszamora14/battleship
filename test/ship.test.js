a12abf808b88642528823c02ea40be18bcb08b1c
Carlos Zamora <carlos14zamora@gmail.com>
2024-04-29 00:22:31 -0600

    add test for checkForShip function to correctly report no ship at specified player coordinate

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,7 +1,14 @@
 const expect = require('chai').expect;
+const { checkForShip } = require('../game_logic/ship');
 
 describe('checkForShip', function () {
-  it ('should correctly report no ship at a given player coordinate', function () {
+  it('should correctly report no ship at a given player coordinate', function () {
+    const player = {
+      ships: [
+        { locations: [[0, 0]] }
+      ]
+    };
 
-  })
+    expect(checkForShip(player, [9, 9])).to.be.false;
+  });
 });
