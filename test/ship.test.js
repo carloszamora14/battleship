ce5ea436878181034f1d625351ae89a20c487994
Carlos Zamora <carlos14zamora@gmail.com>
2024-05-01 23:02:41 -0600

    refactor test suite to utilize Mocha's before hooks

--- test/ship.test.js
+++ test/ship.test.js
@@ -2,46 +2,32 @@ const expect = require('chai').expect;
 const { checkForShip, damageShip, fire } = require('../game_logic/ship');
 
 describe('checkForShip', function () {
-  it('should correctly report no ship at a given player coordinate', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0]] }
-      ]
-    };
+  let player;
 
-    expect(checkForShip(player, [9, 9])).to.be.null;
-  });
-
-  it('should correctly report a ship located at the given coordinate', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0]] }
-      ]
-    };
-
-    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
-  });
-
-  it('should handle ships located at more than one coordinate', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0], [0, 1]] }
-      ]
-    };
-
-    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
-    expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0]);
-  });
-
-  it('should handle checking multiple ships', function () {
-    const player = {
+  before(function () {
+    player = {
       ships: [
         { locations: [[0, 0], [0, 1]] },
         { locations: [[1, 0], [1, 1]] },
         { locations: [[2, 0], [2, 1], [2, 2], [2, 3]] },
       ]
     };
+  });
 
+  it('should correctly report no ship at a given player coordinate', function () {
+    expect(checkForShip(player, [9, 9])).to.be.null;
+  });
+
+  it('should correctly report a ship located at the given coordinate', function () {
+    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
+  });
+
+  it('should handle ships located at more than one coordinate', function () {
+    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
+    expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0]);
+  });
+
+  it('should handle checking multiple ships', function () {
     expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0]);
     expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0]);
 
@@ -67,25 +53,24 @@ describe('damageShip', function () {
 });
 
 describe('fire', function () {
-  it('should not record damage on the given ship', function () {
-    const player = {
+  let player;
+
+  beforeEach(function () {
+    player = {
       ships: [
-        { locations: [[0, 0]], damage: [] }
+        { locations: [[0, 0], [0, 1], [0, 2]], damage: [] },
+        { locations: [[3, 3]], damage: [] },
       ]
     };
+  });
 
-    fire(player, [0, 1]);
+  it('should not record damage on the given ship', function () {
+    fire(player, [0, 9]);
 
     expect(player.ships[0].damage).to.be.empty;
   });
 
   it('should record damage on the given player\'s ship at a given coordinates', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0]], damage: [] }
-      ]
-    };
-
     fire(player, [0, 0]);
 
     expect(player.ships[0].damage).to.not.be.empty;
@@ -93,28 +78,14 @@ describe('fire', function () {
   });
 
   it('should handle multiple ships and register damage only on the ship at the given location', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0]], damage: [] },
-        { locations: [[0, 1]], damage: [] },
-      ]
-    };
-
     fire(player, [0, 1]);
 
-    expect(player.ships[0].damage).to.be.empty;
-    expect(player.ships[1].damage).to.not.be.empty;
-    expect(player.ships[1].damage[0]).to.deep.equal([0, 1]);
+    expect(player.ships[0].damage).to.not.be.empty;
+    expect(player.ships[0].damage[0]).to.deep.equal([0, 1]);
+    expect(player.ships[1].damage).to.be.empty;
   });
 
   it('should correctly register multiple damages at a given location', function () {
-    const player = {
-      ships: [
-        { locations: [[0, 0], [0, 1], [0, 2]], damage: [] },
-        { locations: [[3, 3]], damage: [] },
-      ]
-    };
-
     fire(player, [0, 1]);
     fire(player, [0, 2]);
 
